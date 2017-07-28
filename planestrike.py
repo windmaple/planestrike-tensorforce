868998462d7cf15188d229e07fd546474c253bc8
windmaple <windmaple@gmail.com>
2017-07-28 11:53:12 +0800

    run 4 RL algorithms on PlaneStrike

---
+++ planestrike.py
@@ -0,0 +1,127 @@
+from tensorforce import Configuration
+from tensorforce.agents import *
+from tensorforce.agents import PPOAgent
+from planestrike_env import PlaneStrike
+from tensorforce.execution import Runner
+from tensorforce.core.networks import layered_network_builder
+
+import numpy as np
+import pylab
+
+ts = []
+
+env = PlaneStrike()
+
+agents = []
+agent = VPGAgent(config=Configuration(
+    loglevel='debug',
+    batch_size=1,
+    states=env.states,
+    actions=env.actions,
+    network=layered_network_builder([
+        dict(type='dense', size=32),
+        dict(type='dense', size=32),
+    ]),
+))
+agents.append(agent)
+
+agent = TRPOAgent(config=Configuration(
+    loglevel='info',
+    batch_size=32,
+    # generalized_advantage_estimation=True,
+    # normalize_advantage=False,
+    # gae_lambda=0.97,
+    # override_line_search=False,
+    # cg_iterations=20,
+    # cg_damping=0.01,
+    # line_search_steps=20,
+    # max_kl_divergence=0.005,
+    states=env.states,
+    actions=env.actions,
+    network=layered_network_builder([
+        dict(type='dense', size=32),
+        dict(type='dense', size=32)
+    ])
+))
+agents.append(agent)
+
+agent = PPOAgent(config=Configuration(
+    loglevel='debug',
+    batch_size=32,
+    states=env.states,
+    actions=env.actions,
+    optimizer_batch_size=10,
+    network=layered_network_builder([
+        dict(type='dense', size=32),
+        dict(type='dense', size=32),
+    ]),
+))
+agents.append(agent)
+
+agent = DQNAgent(config=Configuration(
+    loglevel='debug',
+    batch_size=32,
+    states=env.states,
+    actions=env.actions,
+    target_update_frequency=1000,
+    double_dqn=True,
+    network=layered_network_builder([
+        dict(type='dense', size=32),
+        dict(type='dense', size=32)
+    ])
+  # preprocessing=None,
+  # # exploration={
+  # #   type:'epsilon_decay',
+  # #   epsilon:1.0,
+  # #   # epsilon_final=0.1,
+  # #   # epsilon_timesteps=1e6
+  # # },
+  #
+  # memory_capacity=10000,
+  # memory='replay',
+  # update_frequency=4,
+  # first_update=50000,
+  # repeat_update=1,
+  # discount=0.97,
+  # learning_rate=0.00025,
+  # # optimizer={
+  # #   dict(type='rmsprop'),
+  # #   dict(momentum=0.95),
+  # #   dict(epsilon=0.01)
+  # # },
+  # tf_saver=False,
+  # tf_summary=None,
+  #
+  # update_target_weight=1.0,
+  # clip_gradients=0.0,
+))
+agents.append(agent)
+
+
+
+# Callback function printing episode statistics
+def episode_finished(r):
+    print("Finished episode {ep} after {ts} timesteps (reward: {reward})".format(ep=r.episode, ts=r.timestep,
+                                                                                 reward=r.episode_rewards[-1]))
+    ts.append(r.timestep)
+    return True
+
+for agent in agents:
+    # Create the runner
+    runner = Runner(agent=agent, environment=env)
+    # Start learning
+    runner.run(episodes=5000, max_timesteps=50, episode_finished=episode_finished)
+
+    # Print statistics
+    print("Learning finished. Total episodes: {ep}. Average reward of last 100 episodes: {ar}.".format(ep=runner.episode,
+                                                                                                       ar=np.mean(
+                                                                                                           runner.episode_rewards[
+                                                                                                       -100:])))
+    WINDOW_SIZE = 50
+    tmp = [np.mean(runner.episode_rewards[i:i + WINDOW_SIZE]) for i in range(len(runner.episode_rewards) - WINDOW_SIZE)]
+    pylab.plot(tmp, label=agent.name.replace('Agent', ''))
+    # tmp = [np.mean(ts[i:i + WINDOW_SIZE]) for i in range(len(ts) - WINDOW_SIZE)]
+    # pylab.plot(ts)
+
+pylab.legend(loc='lower right')
+pylab.show()
---
+++ planestrike_env.py
@@ -0,0 +1,108 @@
+from random import random
+
+from tensorforce.environments import Environment
+import numpy as np
+import random
+
+N = 36
+S = 8
+
+BOARD_HEIGHT = 6
+BOARD_WIDTH = 6
+BOARD_SIZE = BOARD_HEIGHT * BOARD_WIDTH
+PLANE_SIZE = 8
+
+
+def init_board():
+    hidden_board = np.zeros((BOARD_HEIGHT, BOARD_WIDTH))
+
+    # Populate the plane's position
+    # First figure out the plane's orientation
+    #   0: heading right
+    #   1: heading up
+    #   2: heading left
+    #   3: heading down
+
+    plane_orientation = random.randint(0, 3)
+
+    # Figrue out plane core's position as the '*' below
+    #        |         | |
+    #       -*-        |-*-
+    #        |         | |
+    #       ---
+    if plane_orientation == 0:
+        plane_core_row = random.randint(1, BOARD_HEIGHT - 2)
+        plane_core_column = random.randint(2, BOARD_WIDTH - 2)
+        # Populate the tail
+        hidden_board[plane_core_row][plane_core_column - 2] = 1
+        hidden_board[plane_core_row - 1][plane_core_column - 2] = 1
+        hidden_board[plane_core_row + 1][plane_core_column - 2] = 1
+    elif plane_orientation == 1:
+        plane_core_row = random.randint(1, BOARD_HEIGHT - 3)
+        plane_core_column = random.randint(1, BOARD_WIDTH - 3)
+        # Populate the tail
+        hidden_board[plane_core_row + 2][plane_core_column] = 1
+        hidden_board[plane_core_row + 2][plane_core_column + 1] = 1
+        hidden_board[plane_core_row + 2][plane_core_column - 1] = 1
+    elif plane_orientation == 2:
+        plane_core_row = random.randint(1, BOARD_HEIGHT - 2)
+        plane_core_column = random.randint(1, BOARD_WIDTH - 3)
+        # Populate the tail
+        hidden_board[plane_core_row][plane_core_column + 2] = 1
+        hidden_board[plane_core_row - 1][plane_core_column + 2] = 1
+        hidden_board[plane_core_row + 1][plane_core_column + 2] = 1
+    elif plane_orientation == 3:
+        plane_core_row = random.randint(2, BOARD_HEIGHT - 2)
+        plane_core_column = random.randint(1, BOARD_WIDTH - 2)
+        # Populate the tail
+        hidden_board[plane_core_row - 2][plane_core_column] = 1
+        hidden_board[plane_core_row - 2][plane_core_column + 1] = 1
+        hidden_board[plane_core_row - 2][plane_core_column - 1] = 1
+
+    # Populate the cross
+    hidden_board[plane_core_row][plane_core_column] = 1
+    hidden_board[plane_core_row + 1][plane_core_column] = 1
+    hidden_board[plane_core_row - 1][plane_core_column] = 1
+    hidden_board[plane_core_row][plane_core_column + 1] = 1
+    hidden_board[plane_core_row][plane_core_column - 1] = 1
+
+    return hidden_board.reshape(BOARD_SIZE, )
+
+
+class PlaneStrike(Environment):
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        return 'MinimalTest'
+
+    def close(self):
+        pass
+
+    def reset(self):
+        self.state = np.zeros(N)
+        self.hidden_state = init_board()
+        self.count = 0
+        return self.state
+
+    def execute(self, action):
+        if self.state[action] == 1 and self.state[action] == -1:
+            reward = -1
+        else:
+            if self.hidden_state[action] == 1:
+                self.state[action] = 1
+                self.count = self.count + 1
+                reward = 1
+            else:
+                self.state[action] = -1
+                reward = -1
+        terminal = (self.count == S)
+        return self.state, reward, terminal
+
+    @property
+    def states(self):
+        return dict(shape=(N,), type='float')
+
+    @property
+    def actions(self):
+        return dict(continuous=False, num_actions=N)
